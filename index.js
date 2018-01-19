d964fe39d84f84aaf824c7b5c24db90f1c0ba9e9
webmech <webmech@gmail.com>
2018-01-19 16:39:46 -0500

    feat(*): add peerjs factory implementation that works in nodejs 🎉
    cobble together hack found here: https://github.com/peers/peerjs/issues/103#issuecomment-236054130
    and here: https://github.com/peers/peerjs/issues/103#issuecomment-236352630 to get peerjs working in
    a node app
    
    https://github.com/peers/peerjs/issues/103

---
+++ index.js
@@ -0,0 +1,32 @@
+window = global;
+window.BlobBuilder = require('BlobBuilder');
+
+BinaryPack = require('binary-pack')
+XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+var wrtc = require('electron-webrtc')()
+
+
+RTCPeerConnection = wrtc.RTCPeerConnection;
+RTCSessionDescription = wrtc.RTCSessionDescription;
+RTCIceCandidate = wrtc.RTCIceCandidate;
+
+
+WebSocket = require('ws');
+location = {
+  protocol: 'http'
+};
+
+require('peerjs/lib/exports');
+
+module.exports = function(id, options) {
+  // Deal with overloading :-D
+  if (id && id.constructor == Object) {
+    options = id;
+    id = undefined;
+  } else if (id) {
+    // Ensure id is a string
+    id = id.toString();
+  }
+
+  return new Peer(id, options);
+};
