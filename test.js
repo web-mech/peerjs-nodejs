db79989f04ccc8921cc8b66d7deb00240a65dccf
webmech <webmech@gmail.com>
2018-01-20 15:25:38 -0500

    test(*): add working test for basic connection

--- test.js
+++ test.js
@@ -3,14 +3,10 @@ import test from 'ava';
 const peerJs = require('.');
 
 test(async t => {
-  const bar = Promise.resolve('bar');
-
-  const peer = new Promise((resolve, reject) => {
-    let p = peerJs();//add options here
+  await new Promise((resolve, reject) => {
+    let p = peerJs({ key: 'ut2nndo3zqdaq0k9' });//add options here
     p.on('open', () => resolve(p));
+  }).then((peer) => {
+    t.is(peer.open, true);
   });
-  
-  await peer;
-
-  t.is(peer.open, true);
 });
